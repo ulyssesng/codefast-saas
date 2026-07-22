b25d33ed84e9f3ee380b82b18005eaa0057020fe
Ulysses Ng <ulyssesngk@gmail.com>
2026-07-21 18:50:45 -0700

    sign in with AuthJS

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 const ButtonLogin = ({ session, extraStyle }) => {
+  const dashboardUrl = "/dashboard";
+
   if (session) {
     return (
       <Link
-        href="/dashboard"
+        href={dashboardUrl}
         className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
       >
         Welcome back {session.user.name || "friend"}
@@ -12,7 +17,16 @@ const ButtonLogin = ({ session, extraStyle }) => {
     );
   }
 
-  return <button>Login</button>;
+  return (
+    <button
+      className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      onClick={() => {
+        signIn(undefined, { callbackUrl: dashboardUrl });
+      }}
+    >
+      Get started
+    </button>
+  );
 };
 
 export default ButtonLogin;
